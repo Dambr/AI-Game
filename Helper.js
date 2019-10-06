3f585c473eeb0fc9264479a62a22ef73a51cd1df
Dambr <dambr-wolf@mail.ru>
2019-10-06 14:16:41 +0100

    v1.2

--- Helper.js
+++ Helper.js
@@ -1,3 +1,26 @@
-module.exports = function(){
-	console.log("Модуль подключен");
+// Умножение матриц
+function multiplyMatrix(A,B){
+	let rowsA = A.length, colsA = A[0].length,
+		rowsB = B.length, colsB = B[0].length;
+	let C = [];
+	if (colsA != rowsB) return false;
+	for (let i = 0; i < rowsA; i++){
+		C[i] = [];	
+	}
+	for (let k = 0; k < colsB; k++){
+		for (let i = 0; i < rowsA; i++){
+			let temp = 0;
+			for (let j = 0; j < rowsB; j++){
+				temp += A[i][j]*B[j][k];
+			}
+			C[i][k] = temp;
+		}
+	}
+	return C;
+}
+// Транспланирование матрицы
+const transpose = matrix => matrix[0].map((col, i) => matrix.map(row => row[i]));
+
+module.exports = () => {
+	
 }
--- Main.js
+++ Main.js
@@ -10,15 +10,13 @@ const connection = mysql.createConnection({
 const defaultValue = 1 / require('./First').solutions.length;
 const events = require('./First').events;
 const insertQuery = 'Событие VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL, ' + require('./First').solutions.join(` DOUBLE NOT NULL DEFAULT ${defaultValue}, `) + ` DOUBLE NOT NULL DEFAULT ${defaultValue}`;
-// const stb = '\'' + require('./First').stb.join('\', \'') + '\'';
 
 
 new Promise(function(response, reject){
-	connection.query('DROP TABLE test', function(err, res){
+	connection.query('DROP TABLE test', (err, res) => {
 		console.log('Прежние данные стерты');
 		// Конфигурация таблицы
-		connection.query('CREATE TABLE test(' + 'id INT AUTO_INCREMENT PRIMARY KEY, ' + insertQuery + ')',
-		function(err, res){
+		connection.query('CREATE TABLE test(' + 'id INT AUTO_INCREMENT PRIMARY KEY, ' + insertQuery + ')', (err, res) => {
 			if (err) throw err;
 			console.log('Новые данные сконфигурированы');
 			response();
@@ -29,19 +27,22 @@ new Promise(function(response, reject){
 	() => {
 		// Заполнение таблицы новыми данными
 		for (let event of events){
-			connection.query('INSERT INTO test(Событие) VALUES(\'' + event + '\')');
+			connection.query('INSERT INTO test(Событие) VALUES(\'' + event + '\')', (err, res) => {
+				if (err) throw err;
+			});
 		}
 	}
 )
 .then(
 	() => {
-		require('./Helper.js')();
+		let main = require('./Helper.js');
+		main();
 	}
 )
 .then(
 	() => {
 		// Запись в файл принятия решений
-		connection.query('SELECT * FROM test', function(err, res){
+		connection.query('SELECT * FROM test', (err, res) => {
 			if (err) throw err;
 			fs.writeFileSync('file.json', JSON.stringify(res, null, 4));
 			console.log('Файл записан успешно');
